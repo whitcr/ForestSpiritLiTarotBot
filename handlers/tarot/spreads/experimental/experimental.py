36b3a48fa64e269ffc118e91ee04de2f6d812eff
Denis Lienskyi <whitcr0k@gmail.com>
2025-03-06 01:08:37 +0100

    Fixes

--- handlers/tarot/spreads/experimental/experimental.py
+++ handlers/tarot/spreads/experimental/experimental.py
@@ -56,8 +56,16 @@ async def generate_cards(choices, triplet_type: Union["mtriplet", "striplet"], u
 async def get_choices(user_id, triplet_type: Union["mtriplet", "striplet"]):
     try:
         result = await execute_select(f"SELECT {triplet_type} FROM users WHERE user_id = $1", (user_id,))
+
+        if triplet_type == "mtriplet":
+            if result.count(", ") < 5:
+                raise Exception
+        else:
+            if result.count(", ") < 2:
+                raise Exception
+
         return result.split(', ')
-    except Exception as e:
+    except:
         default_choices = ["raider", "manara", "deviantmoon"]
 
         if triplet_type == "mtriplet":
@@ -67,26 +75,25 @@ async def get_choices(user_id, triplet_type: Union["mtriplet", "striplet"]):
 
 async def send_triplet_image(bot, image, text, date, message, triplet_type: Union["mtriplet", "striplet"]):
     draw_text = ImageDraw.Draw(image)
-    if len(text) == 0:
-        if triplet_type == "mtriplet":
-            draw_text.text((1450, 70), 'МТриплет', font = FONT_XL, fill = 'white')
-        else:
-            draw_text.text((1450, 70), 'СуперТриплет', font = FONT_XL, fill = 'white')
+    width, text_x, label_x = (3200, 1450, 1350) if triplet_type == "mtriplet" else (1770, 740, 655)
+
+    if not text:
+        draw_text.text((text_x, 70), 'МегаТриплет' if triplet_type == "mtriplet" else 'СуперТриплет', font = FONT_XL,
+                       fill = 'white')
     else:
         current_h, pad = 70, 1910
-        para = textwrap.wrap(str(text), width = 100)
-        for line in para:
+        for line in textwrap.wrap(str(text), width = 100):
             w, h = text_size(line, FONT_XL)
-            draw_text.text(((3200 - w) / 2, current_h), line, font = FONT_XL)
+            draw_text.text(((width - w) / 2, current_h), line, font = FONT_XL)
             current_h += h + pad
-    draw_text = ImageDraw.Draw(image)
-    draw_text.text((1350, 1900), 'from @ForestSpiritLi', fill = 'white', font = FONT_XL)
-    draw_text.text((1550, 10), date, fill = 'white', font = FONT_XL)
-    para = textwrap.wrap(f"for {message.from_user.first_name}", width = 30)
+
+    draw_text.text((label_x, 1900), 'from @ForestSpiritLi', fill = 'white', font = FONT_XL)
+    draw_text.text(((width - text_size(date, FONT_XL)[0]) / 2, 10), date, fill = 'white', font = FONT_XL)
+
     current_h, pad = 1950, 10
-    for line in para:
+    for line in textwrap.wrap(f"for {message.from_user.first_name}", width = 30):
         w, h = text_size(line, FONT_S)
-        draw_text.text(((3100 - w) / 2, current_h), line, font = FONT_L)
+        draw_text.text(((width - w) / 2, current_h), line, font = FONT_L)
         current_h += h + pad
 
     reply_to_message_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
@@ -97,139 +104,137 @@ async def send_triplet_image(bot, image, text, date, message, triplet_type: Unio
 @typing_animation_decorator(initial_message = "Раскладываю")
 @router.message(F.text.lower().startswith("мтриплет"), SubscriptionLevel(2))
 async def get_mtriplet(message: types.Message, bot: Bot):
-    # try:
-
-    def generate_image(cards, background_path):
-        w, h = 340, 560
-        x, y = 165, 140
-        col1, col2 = 0, 130
-        color = np.random.randint(col1, col2, size = 6)
-        array = get_gradient_3d(3200, 2000, (color[0], color[1], color[2]),
-                                (color[3], color[4], color[5]),
-                                (True, False, True))
-        image = Image.fromarray(np.uint8(array))
-
-        background = Image.open(background_path)
-        background = background.resize((3200, 2000))
-        background = background.filter(ImageFilter.GaussianBlur(radius = 3))
-        image = Image.blend(image, background, alpha = .3)
-
-        positions = [
-            (6 * x + 5 * w, y + 1200),
-            (5 * x + 4 * w, y + 1200),
-            (4 * x + 3 * w, y + 1200),
-            (6 * x + 5 * w, y + 600),
-            (4 * x + 3 * w, y + 600),
-            (5 * x + 4 * w, y + 600),
-            (5 * x + 4 * w, y),
-            (6 * x + 5 * w, y),
-            (4 * x + 3 * w, y),
-            (3 * x + 2 * w, y + 1200),
-            (2 * x + w, y + 1200),
-            (x, y + 1200),
-            (3 * x + 2 * w, y + 600),
-            (2 * x + w, y + 600),
-            (x, y + 600),
-            (3 * x + 2 * w, y),
-            (2 * x + w, y),
-            (x, y)
-        ]
-
-        for i, position in enumerate(positions):
-            card = Image.open(cards[i])
-            card = card.resize((w, h))
-            image.paste(card, position)
-        return image
-
-    date = datetime.now(pytz.timezone('Europe/Kiev')).strftime("%d.%m")
-
-    user_id = message.from_user.id
-
     try:
-        choices = await get_choices(user_id, "mtriplet")
+
+        def generate_image(cards, background_path):
+            w, h = 340, 560
+            x, y = 165, 140
+            col1, col2 = 0, 130
+            color = np.random.randint(col1, col2, size = 6)
+            array = get_gradient_3d(3200, 2000, (color[0], color[1], color[2]),
+                                    (color[3], color[4], color[5]),
+                                    (True, False, True))
+            image = Image.fromarray(np.uint8(array))
+
+            background = Image.open(background_path)
+            background = background.resize((3200, 2000))
+            background = background.filter(ImageFilter.GaussianBlur(radius = 3))
+            image = Image.blend(image, background, alpha = .3)
+
+            positions = [
+                (6 * x + 5 * w, y + 1200),
+                (5 * x + 4 * w, y + 1200),
+                (4 * x + 3 * w, y + 1200),
+                (6 * x + 5 * w, y + 600),
+                (4 * x + 3 * w, y + 600),
+                (5 * x + 4 * w, y + 600),
+                (5 * x + 4 * w, y),
+                (6 * x + 5 * w, y),
+                (4 * x + 3 * w, y),
+                (3 * x + 2 * w, y + 1200),
+                (2 * x + w, y + 1200),
+                (x, y + 1200),
+                (3 * x + 2 * w, y + 600),
+                (2 * x + w, y + 600),
+                (x, y + 600),
+                (3 * x + 2 * w, y),
+                (2 * x + w, y),
+                (x, y)
+            ]
+
+            for i, position in enumerate(positions):
+                card = Image.open(cards[i])
+                card = card.resize((w, h))
+                image.paste(card, position)
+            return image
+
+        date = datetime.now(pytz.timezone('Europe/Kiev')).strftime("%d.%m")
+
+        user_id = message.from_user.id
+
+        try:
+            choices = await get_choices(user_id, "mtriplet")
+        except Exception as e:
+            choices = ["raider", "manara", "deviantmoon", "ceccoli", "decameron", "vikanimaloracul"]
+
+        background_path = await get_path_background()
+
+        cards = await asyncio.create_task(generate_cards(choices, "mtriplet", user_id))
+
+        image = await asyncio.to_thread(generate_image, cards, background_path)
+
+        text = message.text.split(" ")[1:]
+        text = ' '.join(text)
+
+        await send_triplet_image(bot, image, text, date, message, "mtriplet")
+
+
     except Exception as e:
-        print(f"Error getting choices: {e}")
-        choices = ["raider", "manara", "deviantmoon", "ceccoli", "decameron", "vikanimaloracul"]
+        await message.reply("— Что-то пошло не так, скорее всего у вас выбрано менее 3 колод.")
 
-    background_path = await get_path_background()
-
-    cards = await asyncio.create_task(generate_cards(choices, "mtriplet", user_id))
-
-    image = await asyncio.to_thread(generate_image, cards, background_path)
-
-    text = message.text.split(" ")[1:]
-    text = ' '.join(text)
-
-    await send_triplet_image(bot, image, text, date, message, "mtriplet")
-
-
-# except Exception as e:
-#     await message.reply("— Что-то пошло не так, скорее всего у вас выбрано менее 6 колод.")
 
 @typing_animation_decorator(initial_message = "Раскладываю")
 @router.message(F.text.lower().startswith("стриплет"), SubscriptionLevel(2))
 async def get_striplet(message: types.Message, bot: Bot):
-    # try:
-
-    def generate_image(cards, background_path):
-        w, h = 340, 560
-        x, y = 130, 140
-        col1, col2 = 0, 130
-
-        color = np.random.randint(col1, col2, size = 6)
-        array = get_gradient_3d(1800, 2000, (color[0], color[1], color[2]),
-                                (color[3], color[4], color[5]),
-                                (True, False, True))
-        image = Image.fromarray(np.uint8(array))
-
-        background = Image.open(background_path)
-        background = background.resize((1800, 2000))
-        background = background.filter(ImageFilter.GaussianBlur(radius = 3))
-        image = Image.blend(image, background, alpha = .3)
-
-        positions = [
-            (3 * x + 2 * w + 110, y + 1200),
-            (2 * x + w + 110, y + 1200),
-            (x + 110, y + 1200),
-            (3 * x + 2 * w + 110, y + 600),
-            (2 * x + w + 110, y + 600),
-            (x + 110, y + 600),
-            (3 * x + 2 * w + 110, y),
-            (2 * x + w + 110, y),
-            (x + 110, y)
-        ]
-
-        for i, position in enumerate(positions):
-            card = Image.open(cards[i])
-            card = card.resize((w, h))
-            image.paste(card, position)
-
-        return image
-
-    date = datetime.now(pytz.timezone('Europe/Kiev')).strftime("%d.%m")
-    msg = await message.reply("Ваш супертриплет генерируется...")
-    user_id = message.from_user.id
-
     try:
-        choices = await get_choices(user_id, "striplet")
+
+        def generate_image(cards, background_path):
+            w, h = 340, 560
+            x, y = 130, 140
+            col1, col2 = 0, 130
+
+            color = np.random.randint(col1, col2, size = 6)
+            array = get_gradient_3d(1800, 2000, (color[0], color[1], color[2]),
+                                    (color[3], color[4], color[5]),
+                                    (True, False, True))
+            image = Image.fromarray(np.uint8(array))
+
+            background = Image.open(background_path)
+            background = background.resize((1800, 2000))
+            background = background.filter(ImageFilter.GaussianBlur(radius = 3))
+            image = Image.blend(image, background, alpha = .3)
+
+            positions = [
+                (3 * x + 2 * w + 110, y + 1200),
+                (2 * x + w + 110, y + 1200),
+                (x + 110, y + 1200),
+                (3 * x + 2 * w + 110, y + 600),
+                (2 * x + w + 110, y + 600),
+                (x + 110, y + 600),
+                (3 * x + 2 * w + 110, y),
+                (2 * x + w + 110, y),
+                (x + 110, y)
+            ]
+
+            for i, position in enumerate(positions):
+                card = Image.open(cards[i])
+                card = card.resize((w, h))
+                image.paste(card, position)
+
+            return image
+
+        date = datetime.now(pytz.timezone('Europe/Kiev')).strftime("%d.%m")
+        user_id = message.from_user.id
+
+        try:
+            choices = await get_choices(user_id, "striplet")
+        except:
+            choices = ["raider", "manara", "deviantmoon"]
+
+        background_path = await get_path_background()
+
+        cards = await asyncio.create_task(generate_cards(choices, "striplet", user_id))
+
+        image = await asyncio.to_thread(generate_image, cards, background_path)
+
+        text = message.text.split(" ")[1:]
+        text = ' '.join(text)
+
+        await send_triplet_image(bot, image, text, date, message, "striplet")
+
+
     except Exception as e:
-        choices = ["raider", "manara", "deviantmoon"]
-
-    background_path = await get_path_background()
-
-    cards = await asyncio.create_task(generate_cards(choices, "striplet", user_id))
-
-    image = await asyncio.to_thread(generate_image, cards, background_path)
-
-    text = message.text.split(" ")[1:]
-    text = ' '.join(text)
-
-    await send_triplet_image(bot, image, text, date, message, "striplet")
-    await delete_message(msg)
-
-
-# except Exception as e:
-#     await message.reply("— Что-то пошло не так, скорее всего у вас выбрано менее 3 колод.")
+        await message.reply("— Что-то пошло не так, скорее всего у вас выбрано менее 3 колод.")
 
 
 @router.message(F.text.lower().startswith("настройки"), SubscriptionLevel(2))
