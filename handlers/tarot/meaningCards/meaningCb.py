4e25c79f31f6f6c24994d89ac8393e1b60a69392
Denis Lienskyi <whitcr0k@gmail.com>
2025-03-07 18:42:12 +0100

    Fixes

--- handlers/tarot/meaningCards/meaningCb.py
+++ handlers/tarot/meaningCards/meaningCb.py
@@ -20,26 +20,24 @@ async def generate_meaning_keyboard(user_choice, callback_data):
     return builder.as_markup()
 
 
-@router.callback_query(F.data.startswith('meaningCards'))
+@router.callback_query(F.data.startswith('meaning_'))
 async def meaning_cb(call: types.CallbackQuery, bot: Bot):
-    try:
-        await call.answer()
-        num = await get_data(call.message.message_id)
-        theme = call.data.split('_')[2]
-        choice = call.data.split('_')[1]
-        table = f"meaning_{choice}"
-        keyboard = await generate_meaning_keyboard(choice, call.data)
-        meaning_text = await execute_select("SELECT {} FROM {} WHERE number = {}", (theme, table, num))
+    await call.answer()
+    num = await get_data(call.message.message_id)
+    theme = call.data.split('_')[2]
+    choice = call.data.split('_')[1]
+    table = f"meaning_{choice}"
+    keyboard = await generate_meaning_keyboard(choice, call.data)
+    print(f"SELECT {theme} FROM {table} WHERE number = {num}")
+    meaning_text = await execute_select(f"SELECT {theme} FROM {table} WHERE number = $1;", (num,))
+    print(meaning_text)
+    if len(meaning_text) > 4096:
+        meaning_text = meaning_text[:4000]
 
-        if len(meaning_text) > 4096:
-            meaning_text = meaning_text[:4000]
+    text = meaning_text + f"\n\n<a href = '{MEANINGS_BUTTONS[choice]['source']}'>Источник</a>"
 
-        text = meaning_text + f"\n\n<a href = '{MEANINGS_BUTTONS[choice]['source']}'>Источник</a>"
-
-        await bot.edit_message_text(chat_id = call.message.chat.id, message_id = call.message.message_id,
-                                    text = text, reply_markup = keyboard)
-    except:
-        pass
+    await bot.edit_message_text(chat_id = call.message.chat.id, message_id = call.message.message_id,
+                                text = text, reply_markup = keyboard)
 
 
 @router.callback_query(F.data == 'menu_return')
